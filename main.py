374115d0114234d8fa2db04bda66c90a35587c7d
Oleksii Pavliuk <capetruha@gmail.com>
2023-02-27 22:59:22 +1100

    add

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 import sqlalchemy
 import json
 from datetime import date, datetime
+import functions_framework
 
 
-
+# Get Database connection
 def getDB():
-
   connection_name='optimal-life-378201:australia-southeast1:todo-database'
   db_name='todo'
   db_user='postgres'
@@ -36,6 +36,23 @@ def json_serial(obj):
         return obj.isoformat()
     raise TypeError ("Type %s not serializable" % type(obj))
 
+
+
+
+# Send headers 
+def send_headers(request): 
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+        print('OPTIONS SENT')
+        return ('', 204, headers)
+
+
 #############################
 # 
 #       USER FUNCTIONS
@@ -45,6 +62,15 @@ def json_serial(obj):
 
 # Get users V
 def getUsers(request):
+    
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+    }
 
     stmt = sqlalchemy.text('SELECT * FROM users')
     
@@ -57,18 +83,28 @@ def getUsers(request):
           return json.dumps([dict(row) for row in rows]), 200, {'Content-Type': 'application/json'}
     except Exception as e:
         error_msg = "An error occurred while retrieving data: {}".format(str(e))
-        return json.dumps({"error": error_msg}), 500, {'Content-Type': 'application/json'}
+        return json.dumps({"error": error_msg}), 500,headers
 
 
 # Add user V
 def addUser(request):
-  # Get request params
-    params = request.get_json()
-    username = params['username']
-    password = params['password']
 
-    if( not username and not password):
-      return 'Wrong request'
+    if request.method == 'OPTIONS':    
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
+
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json :
+              username = request_json['username']
+              password = request_json['password']
+        else:
+            raise ValueError("JSON is invalid, or missing property")
+
+    headers = {
+            'Access-Control-Allow-Origin': '*'
+    }
 
     # Execute query
     stmt = sqlalchemy.text('insert into users (username, password) values (:username, :password)')
@@ -80,19 +116,29 @@ def addUser(request):
         with db.connect() as conn:
           result = conn.execute(stmt)
     except Exception as e:
-        return 'Error: {}'.format(str(e)),500
-    return 'ok', 200
+        return 'Error: {}'.format(str(e)),500,headers
+    return 'ok', 200,headers
 
 
 # Check user
 def checkUser(request):
-  # Get request params
-    params = request.get_json()
-    username = params['username']
-    password = params['password']
+    
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
 
-    if( not username and not password):
-      return 'Wrong request'
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json :
+              username = request_json['username']
+              password = request_json['password']
+        else:
+            raise ValueError("JSON is invalid, or missing property")
+
+    headers = {
+            'Access-Control-Allow-Origin': '*'
+    }
 
     # Execute query
     stmt = sqlalchemy.text('SELECT * FROM users WHERE username = :username')
@@ -108,21 +154,31 @@ def checkUser(request):
                 return ("bad request"), 400
     except Exception as e:
         return 'Error: {}'.format(str(e)),500
-    return 'ok', 200
+    return 'ok', 200,headers
 
 
 # Delete user V
 def deleteUser(request):
-  # Get request params
-    params = request.get_json()
-    username = params['username']
+    
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
 
-    if( not username):
-      return 'Wrong request'
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'id' in request_json:
+          id = request_json['id']
+        else:
+            raise ValueError("JSON is invalid, or missing property")
+
+    headers = {
+            'Access-Control-Allow-Origin': '*'
+    }
 
     # Execute query
-    stmt = sqlalchemy.text('DELETE FROM users WHERE username = :username')
-    stmt = stmt.bindparams(username=username)
+    stmt = sqlalchemy.text('DELETE FROM users WHERE id = :id')
+    stmt = stmt.bindparams(id=id)
     
     db = getDB()
     try:
@@ -131,7 +187,7 @@ def deleteUser(request):
           result = conn.execute(stmt)
     except Exception as e:
         return 'Error: {}'.format(str(e)),500
-    return 'ok', 200
+    return 'ok', 200,headers
 
 
 ##############################
@@ -141,44 +197,72 @@ def deleteUser(request):
 ##############################
 
 # Get tasks V
+@functions_framework.http
 def getTasks(request):
 
-    params = request.get_json()
-    user_id = params['user_id']
 
-    if( not user_id):
-      return 'Wrong request'
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
+
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'user_id' in request_json:
+            user_id = request_json['user_id']
+        else:
+            raise ValueError("JSON is invalid, or missing a 'user_id' property")
+    print(str(user_id) + " extracted ")
+
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600'
+    }
 
     stmt = sqlalchemy.text('SELECT * FROM tasks WHERE user_id = :user_id')
-    stmt = stmt.bindparams( user_id=user_id )
-    
+    stmt = stmt.bindparams(user_id=user_id)
     db = getDB()
+
     try:
-        # Exequte query and send responce
+        # Execute query and send response
         with db.connect() as conn:
-          result = conn.execute(stmt)
-          rows = result.fetchall()
-          return json.dumps({"data": [dict(row) for row in rows]}, default=json_serial), 200, {'Content-Type': 'application/json'}
+            result = conn.execute(stmt)
+            rows = result.fetchall()
+            print("Data extracted, sending")
+            response = json.dumps({"data": [dict(row) for row in rows]}, default=json_serial)
+        return(response,200,headers)
     except Exception as e:
         error_msg = "An error occurred while retrieving data: {}".format(str(e))
-        return json.dumps({"error": error_msg}), 500, {'Content-Type': 'application/json'}
-
+        response = json.dumps({"error": error_msg})
+        return (response, 500, headers)
 
 # Add task V
 def addTask(request):
-  # Get request params
-    params = request.get_json()
-    name = params['name']
-    description = params['description']
-    user_id = params['user_id']
+    
 
-    if( not user_id and not name and not description):
-      return 'Wrong request'
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
+
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'user_id' in request_json:
+          name = request_json['name']
+          description = request_json['description']
+          user_id = request_json['user_id']
+        else:
+            raise ValueError("JSON is invalid, or missing property")
+
+    headers = {
+            'Access-Control-Allow-Origin': '*'
+    }
 
     # Execute query
     stmt = sqlalchemy.text('insert into tasks (name, description, user_id ) values (:name, :description, :user_id)')
     stmt = stmt.bindparams(name=name , description = description, user_id = user_id)
-    
     db = getDB()
     try:
         # Exequte query and send responce
@@ -186,18 +270,31 @@ def addTask(request):
           result = conn.execute(stmt)
     except Exception as e:
         return 'Error: {}'.format(str(e)),500
-    return 'ok', 200
+    return 'ok', 200,headers
 
 
 
 #Edit task V
 def editTask(request):
   # Get request params
-    params = request.get_json()
-    id = params['id']
-    name = params['name']
-    description = params['description']
+    
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
 
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json:
+          name = request_json['name']
+          description = request_json['description']
+          id = request_json['id']
+        else:
+            raise ValueError("JSON is invalid, or missing property")
+
+    headers = {
+            'Access-Control-Allow-Origin': '*'
+    }
 
     # Execute query
     if( not id and not name and  not description):
@@ -219,17 +316,29 @@ def editTask(request):
           result = conn.execute(stmt)
     except Exception as e:
         return 'Error: {}'.format(str(e)),500
-    return 'ok', 200
+    return 'ok', 200,headers
 
 
 
 #Change task( make it done)  V
 def changeTask(request):
-  # Get request params
-    params = request.get_json()
-    id = params['id']
-    if( not id):
-      return 'Wrong request'
+
+
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
+
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'id' in request_json:
+          id = request_json['id']
+        else:
+            raise ValueError("JSON is invalid, or missing property")
+
+    headers = {
+            'Access-Control-Allow-Origin': '*'
+    }
 
     # Execute query
     stmt = sqlalchemy.text('UPDATE tasks SET done = true, done_date = CURRENT_TIMESTAMP WHERE id = :id;')
@@ -242,18 +351,28 @@ def changeTask(request):
           result = conn.execute(stmt)
     except Exception as e:
         return 'Error: {}'.format(str(e)),500
-    return 'ok', 200
+    return 'ok', 200,headers
 
 
 
 #Translate task(log translation)  V
 def translateTask(request):
-  # Get request params
-    params = request.get_json()
-    id = params['id']
 
-    if( not id):
-      return 'Wrong request'
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
+
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'id' in request_json:
+          id = request_json['id']
+        else:
+            raise ValueError("JSON is invalid, or missing property")
+
+    headers = {
+            'Access-Control-Allow-Origin': '*'
+    }
 
     # Execute query
     stmt = sqlalchemy.text('UPDATE tasks SET translated = true, translated_date = CURRENT_TIMESTAMP WHERE id = :id;')
@@ -266,18 +385,28 @@ def translateTask(request):
           result = conn.execute(stmt)
     except Exception as e:
         return 'Error: {}'.format(str(e)),500
-    return 'ok', 200
+    return 'ok', 200,headers
 
 
 
 #Delete task V 
 def deleteTask(request):
-  # Get request params
-    params = request.get_json()
-    id = params['id']
+  
+    if request.method == 'OPTIONS':
+      send_headers(request)
+    print("GETTING PARAMS")
+    content_type = request.headers['content-type']
 
-    if( not id):
-      return 'Wrong request'
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'id' in request_json:
+          id = request_json['id']
+        else:
+            raise ValueError("JSON is invalid, or missing property")
+
+    headers = {
+            'Access-Control-Allow-Origin': '*'
+    }
     # Execute query
     stmt = sqlalchemy.text('UPDATE tasks SET deleted=true WHERE id = :id;')
     stmt = stmt.bindparams(id=id)
@@ -289,4 +418,4 @@ def deleteTask(request):
           result = conn.execute(stmt)
     except Exception as e:
         return 'Error: {}'.format(str(e)),500
-    return 'ok', 200
+    return 'ok', 200,headers
